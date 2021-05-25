9296eb7e4aedcd6f7c2d3e7b9ef64e7d3b823f1f
Tatiana Robert <trobert@MacBook-Pro-de-Tatiana.home>
2021-05-25 20:34:55 +0200

    :tada:  Boilerplate for Floema's project - global structure

---
+++ webpack.config.js
@@ -0,0 +1,143 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+// Résolution d'une erreur lors du npm start associé au module target:'node'
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
+
+const dirApp = path.join(__dirname, 'app')
+const dirShared = path.join(__dirname, 'shared')
+const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'nodeModules'
+
+// console.log(dirApp, dirShared, dirStyles)
+
+module.exports = {
+  entry: [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
+
+  resolve: {
+    modules: [
+      dirApp,
+      dirShared,
+      dirStyles,
+      dirNode
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }]
+        ]
+      }
+    }),
+
+    new CleanWebpackPlugin()
+
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+
+          },
+          {
+            loader: 'postcss-loader'
+
+          },
+          {
+            loader: 'sass-loader'
+
+          }
+        ]
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|web)$/,
+        loader: 'file-loader',
+        options: {
+          // eslint-disable-next-line space-before-function-paren
+          name(file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader
+          }
+        ]
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
+
+  target: 'node'
+  // Résolution de l'erreur au npm start (erreur qu'il n'avait pas dans sa console)
+}
